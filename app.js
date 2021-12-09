42846c4eacf655841c560fad97e25f1257411dcd
Mai-ly <mai-ly.lehoux@zenika.com>
2021-12-09 15:56:31 +0100

    :see_no_evil: push some requests issues

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-const { App } = require("@slack/bolt");
+const { App, ExpressReceiver } = require("@slack/bolt");
 const request = require("request");
 const qs = require("qs");
 require("dotenv").config();
 const { Octokit } = require("@octokit/core");
+const fetch = require("node-fetch");
+const { createOrUpdateTextFile } = require("@octokit/plugin-create-or-update-text-file")
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+const expressReceiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-const hookPersonalMessages = 'T02LBG2Q75Y/B02NMKRLUUV/rw5LEm7xefkuLz0yiIcKF7vL';
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  receiver: expressReceiver
+});
 
+const hookPersonalMessages = process.env.SLACK_WEBHOOK_MESSAGES;
 
+app.event('bot_added', async ({ event, client, context }) => {
+  try {
+    const result = await app.client.chat.postMessage({
+      app_id: token,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Nice to meet you, i am the Skillz bot. I will keep you updated about the new releases of the Skillz app, and you can use my commands to progress in Skillz !'
+          },
+        }
+      ],
+      // Text in the notification
+      text: 'Message from Test App'
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+  console.log(result)
+});
 
 app.event('app_home_opened', async ({ event, client, context }) => {
   try {
@@ -94,6 +122,18 @@ app.command('/helloworld', async ({ ack, payload, context }) => {
   }
 });
 
+/* var response = fetch(("https://slack.com/api/users.lookupByEmail?token=" + TOKEN + "&email=" + email), options);
+*/
+const slackLookupByEmail = async function (email) {
+  const result = await app.client.users.lookupByEmail({
+    token: process.env.SLACK_BOT_TOKEN,
+    email: 'mai-ly.lehoux@zenika.com'
+  });
+  const userInformation = JSON.parse(result)
+  console.log(userInformation.user.id)
+  return userInformation.user.id
+}
+
 app.action('button_abc', async ({ ack, body, context }) => {
   // Acknowledge the button request
   ack();
@@ -123,7 +163,7 @@ app.action('button_abc', async ({ ack, body, context }) => {
   }
 });
 
-const octokit = new Octokit({ auth: "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" });
+const octokit = new Octokit({ auth: process.env.GITHUB_AUTH });
 
 const getData = async function () {
   const res = await octokit.request("GET /repos/{owner}/{repo}/releases", {
@@ -134,17 +174,27 @@ const getData = async function () {
   return resJSON
 };
 
+
+const old = async function () {
+  const res = await octokit.request(`POST /payload`, {
+  });
+  const resJSON = JSON.parse(res);
+  return resJSON
+};
+
 (async () => {
   await app.start(process.env.PORT || 3000);
+  const test = slackLookupByEmail("mai-ly.lehoux@zenika.com")
   try {
-    const releaseInfo = await getData();
-    console.log(releaseInfo)
+    //const releaseInfo = await getData();
+    //const ui = await old(); 
+    //console.log("release : " + releaseTrigger)
     const slackBodyGithubMessage = {
       mkdwn: true,
-      text: `*${releaseInfo}*`,
+      text: `*${test}*`,
       attachments: ({
         color: 'good',
-        text: `*${releaseInfo}*`
+        text: " "
       })
     }
 
