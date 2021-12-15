75a0ac40b33d6f184cbd68b725063e58c0e67582
Mai-ly <mai-ly.lehoux@zenika.com>
2021-12-15 17:44:23 +0100

    :test-tube: add tests for getInformations file

--- src/lib/getInformations.test.js
+++ src/lib/getInformations.test.js
@@ -1,6 +1,6 @@
-const { getChannelID } = require("./getInformations");
+const { getChannelID, getUserID } = require("./getInformations");
 
-const myApp = {
+const myAppClientConversationIsOk = {
   client: {
     conversations: {
       open: jest.fn().mockResolvedValue({
@@ -13,10 +13,93 @@ const myApp = {
   },
 };
 
-it("test GetChannelID", async () => {
-  const result = await getChannelID("testID", myApp, "testToken");
+const myAppClientConversationIsNotOk = {
+  client: {
+    conversations: {
+      open: jest.fn().mockResolvedValue({
+        ok: false,
+      }),
+    },
+  },
+};
+
+const myAppClientUsersIsOk = {
+  client: {
+    users: {
+      lookupByEmail: jest.fn().mockResolvedValue({
+        ok: true,
+        user: "result",
+      }),
+    },
+  },
+};
+
+const myAppClientUsersIsNotOk = {
+  client: {
+    users: {
+      lookupByEmail: jest.fn().mockResolvedValue({
+        ok: false,
+      }),
+    },
+  },
+};
+
+it("test GetChannelID return value success", async () => {
+  const result = await getChannelID(
+    "testID",
+    myAppClientConversationIsOk,
+    "testToken"
+  );
   expect(result).toBe("result");
 });
-it("test Informations", async () => {
-  expect((await myApp.client.conversations.open()).ok).toBe(true);
+
+it("test GetChannelID return value fail", async () => {
+  const result = await getChannelID(
+    "testID",
+    myAppClientConversationIsNotOk,
+    "testToken"
+  );
+  expect(result).toBe("");
+});
+
+it("test Informations myAppClientConversationIsOk", async () => {
+  expect(
+    (await myAppClientConversationIsOk.client.conversations.open()).ok
+  ).toBe(true);
+});
+
+it("test Informations myAppClientConversationIsNotOk", async () => {
+  expect(
+    (await myAppClientConversationIsNotOk.client.conversations.open()).ok
+  ).toBe(false);
+});
+
+it("the GetChannelID fetch fails with an error", async () => {
+  try {
+    await getChannelID("testID", myAppClientConversationIsNotOk, "testToken");
+  } catch (e) {
+    expect(e).toMatch("error");
+  }
+});
+
+it("the getUserID fetch fails with an error", async () => {
+  try {
+    await getUserID("testID", myAppClientUsersIsNotOk, "testToken");
+  } catch (e) {
+    expect(e).toMatch("error");
+  }
+});
+
+it("test getUserID return value success", async () => {
+  const result = await getUserID("testID", myAppClientUsersIsOk, "testToken");
+  expect(result).toBe("result");
+});
+
+it("the getUserID return fails", async () => {
+  const result = await getUserID(
+    "testID",
+    myAppClientUsersIsNotOk,
+    "testToken"
+  );
+  expect(result).toBe("");
 });
