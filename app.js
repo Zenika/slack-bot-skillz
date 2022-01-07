8a4d82d7d662d014656e5ef2cb9462177295b02b
Mai-ly <mai-ly.lehoux@zenika.com>
2022-01-07 16:20:08 +0100

    add modal to note skillz -> front only

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const { homePage } = require("./src/home");
 const { getUserID } = require("./src/lib/getSlackInformations");
 const { getRequest } = require("./src/lib/getRequest");
 const { sendWelcomeMessage } = require("./src/sendMessagesToSkillsUsers");
-const {reminderNoteSkillz} = require("./src/reminderNoteSkillz")
-
+const { reminderNoteSkillz } = require("./src/reminderNoteSkillz");
+const { actionsHandler } = require("./src/actions");
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -18,8 +18,9 @@ module.exports = app;
   //getUserID("mai-ly.lehoux@zenika.com", app, app.signingSecret);
   //getRequest("http://localhost:8080/api/rest/get-all-users");
   reminderNoteSkillz();
-  sendWelcomeMessage(app, app.token);
+  //sendWelcomeMessage(app, app.token);
   homePage(app);
+  actionsHandler(app);
   commandsHandler(app);
   debugger;
 })();
--- src/responseCommandByTopic.js
+++ src/responseCommandByTopic.js
@@ -1,20 +1,20 @@
 const { verifyAgencyIsGiven } = require("./lib/verifyAgencyIsGiven");
 const { verifyTopicIsGiven } = require("./lib/verifyTopicIsGiven");
-const {getRequest} = require("./lib/getRequest")
+const { getRequest } = require("./lib/getRequest");
 async function responseCommandByTopic(text) {
   let names = "";
   let agency = await verifyAgencyIsGiven(text);
-  if (agency === "")
-    return "fail";
+  if (agency === "") return "fail";
   let topic = await verifyTopicIsGiven(text);
-  if (topic === "")
-    return "fail";
-  
+  if (topic === "") return "fail";
+
   const response = await getRequest(
-     `https://staging.hasura.skillz.zenika.com/api/rest/get-users-by-agency-and-topics?agency=${agency}&topic=${topic}`
+    `https://staging.hasura.skillz.zenika.com/api/rest/get-users-by-agency-and-topics?agency=${agency}&topic=${topic}`
   );
   for (let i = 0; i < response.User.length; i++) {
-    names = names + `:bulb: *${response.User[i].name}* liked ${topic} ! watch his or her profile there thanks to the link below \n :mag: https://skillz.zenika.com/profile/${response.User[i].email}\n\n\n`
+    names =
+      names +
+      `:bulb: *${response.User[i].name}* liked ${topic} ! watch his or her profile there thanks to the link below \n :mag: https://skillz.zenika.com/profile/${response.User[i].email}\n\n\n`;
   }
   return names;
 }
