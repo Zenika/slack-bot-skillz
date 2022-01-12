784bbd4bc0737a85adf6bc9c4b44f3b33dd8b57b
Ked57 <kedketh@gmail.com>
2022-01-12 17:38:40 +0100

    Try to use express so we can add routes to call

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const http = require("http");
-const { App } = require("@slack/bolt");
+const { App, ExpressReceiver } = require("@slack/bolt");
+const express = require("express");
 const { commandsHandler } = require("./src/commands");
 const { homePage } = require("./src/home");
 const { getUserID } = require("./src/lib/getSlackInformations");
@@ -7,32 +7,32 @@ const { getRequest } = require("./src/lib/getRequest");
 const { sendWelcomeMessage } = require("./src/sendMessagesToSkillsUsers");
 //const { reminderNoteSkillz } = require("./src/reminderNoteSkillz");
 const { actionsHandler } = require("./src/actions");
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+
+const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  endpoints: {
+    events: `/slack/events`,
+  },
 });
-module.exports = app;
+const boltApp = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver,
+});
+module.exports = boltApp;
 
 const port = process.env.PORT || 8000;
-const slackPort = process.env.SLACK_PORT || 4000;
 
 (async () => {
-  await app.start(slackPort);
-  console.log("Server started");
+  const app = express();
+  app.use("/", (_, res) => res.end());
+  app.use(receiver.router);
   //getUserID("mai-ly.lehoux@zenika.com", app, app.signingSecret);
   //getRequest("http://localhost:8080/api/rest/get-all-users");
   //reminderNoteSkillz();
   //sendWelcomeMessage(app, app.token);
-  homePage(app);
-  actionsHandler(app);
-  commandsHandler(app);
+  homePage(boltApp);
+  actionsHandler(boltApp);
+  commandsHandler(boltApp);
   debugger;
-  // This is for health checks by clever cloud
-  const server = http.createServer((req, res) => {
-    res.end();
-  });
-  server.on("clientError", (err, socket) => {
-    socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
-  });
-  server.listen(port);
+  app.listen(port, () => console.log("App started listening on port", port));
 })();
