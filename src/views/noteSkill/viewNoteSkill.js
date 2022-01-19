c378c4662e904bf3c33c4990ee70a8c6a9688f51
Mai-ly <mai-ly.lehoux@zenika.com>
2022-01-19 16:24:02 +0100

    :sparkles: add a reponse message when we note a skill

--- src/views/noteSkill/viewNoteSkill.js
+++ src/views/noteSkill/viewNoteSkill.js
@@ -1,8 +1,12 @@
-const { getUserEmail } = require("../../lib/bolt/getSlackInformations");
+const { getUserEmail, getChannelID } = require("../../lib/bolt/getSlackInformations");
 const { request } = require("../../lib/utils/request");
+const { postMessage } = require("../../lib/bolt/postMessages");
+const { getSkillNameAndCategory } = require("../../lib/requestsHasura/getSkillInfos")
 
 module.exports = {
   viewNoteSkill(app) {
+      const skillIDTesting = "0ffd1717-d46b-4a76-8dec-548505c18fcb";
+
     app.view("noteSkill", async ({ ack, body, view }) => {
       await ack();
       const skillValue =
@@ -17,9 +21,12 @@ module.exports = {
       try {
         const userEmail = await getUserEmail(user, app, app.token);
         await request(
-          `${process.env.HASURA_BASE_URL}/api/rest/update-skill?email=${userEmail}&skillId=0ffd1717-d46b-4a76-8dec-548505c18fcb&skillLevel=${skillValue}&desireLevel=${desireValue}`,
+          `${process.env.HASURA_BASE_URL}/api/rest/update-skill?email=${userEmail}&skillId=${skillIDTesting}&skillLevel=${skillValue}&desireLevel=${desireValue}`,
           "PUT"
         );
+        const channelID = await getChannelID(user, app, app.token);
+        const skillName = await getSkillNameAndCategory(skillIDTesting);
+        await postMessage(channelID, `:tada: You updated your desire and skill level of *${skillName.name}* :tada:\n\n:bar_chart: Watch the graph of this skill here https://skillz.zenika.com/skills/mine/${skillName.Category.label}`, app, app.token)
       } catch (e) {
         console.error("error", e);
       }
