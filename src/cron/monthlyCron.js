9271fd56ed25874fc2bfacdecb13365b4ecff600
MailyLehoux <85115963+MailyLehoux@users.noreply.github.com>
2022-05-11 16:07:30 +0200

    condition for beta testers (#53)
    * condition for beta testers
    
    * :sparkles: new cron. Can be test if we delete the beta testers list

--- src/cron/monthlyCron.js
+++ src/cron/monthlyCron.js
@@ -2,14 +2,22 @@ var cron = require("node-cron");
 const { arrayOfDelayedSkillsByAllUsers } = require("./arraysOfDelayedSkills");
 
 async function monthlyCron(app) {
-  if (process.env.ENV === "DEV" || process.env.ENV === "STAGING") {
+  if (process.env.ENV === "DEV") {
     cron.schedule("* * * * *", () => {
       arrayOfDelayedSkillsByAllUsers(app);
     });
-  } else {
-    cron.schedule("0 14 1 * *", () => {
+  } else if (process.env.BETA_TESTS) {
+    cron.schedule("0 0 0 * *", () => {
       arrayOfDelayedSkillsByAllUsers(app);
     });
+  } else {
+    cron.schedule("0 0 0 * *", () => {
+      arrayOfDelayedSkillsByAllUsers(app);
+    });
+    //send message once monthly. Uncomment once the beta tests are done
+    /*     cron.schedule("0 14 1 * *", () => {
+      arrayOfDelayedSkillsByAllUsers(app);
+    }); */
   }
 }
 
