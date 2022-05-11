8f030628b8897e5c55ea5a805347f0d5652fc957
Mai-ly <mai-ly.lehoux@zenika.com>
2022-05-11 11:14:46 +0200

    condition for beta testers

--- src/cron/monthlyCron.js
+++ src/cron/monthlyCron.js
@@ -2,8 +2,13 @@ var cron = require("node-cron");
 const { arrayOfDelayedSkillsByAllUsers } = require("./arraysOfDelayedSkills");
 
 async function monthlyCron(app) {
-  if (process.env.ENV === "DEV" || process.env.ENV === "STAGING") {
+  if (process.env.ENV === "DEV") {
     cron.schedule("* * * * *", () => {
+      arrayOfDelayedSkillsByAllUsers(app);
+    });
+  }
+  if (process.env.BETA_TESTS) {
+    cron.schedule("0 0 0 * *", () => {
       arrayOfDelayedSkillsByAllUsers(app);
     });
   } else {
