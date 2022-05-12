6b596ed781a5647b8a5ba70aa4a854f320907df6
Mai-ly <mai-ly.lehoux@zenika.com>
2022-05-12 10:48:30 +0200

    :sparkles: add Paris timezone

--- src/cron/monthlyCron.js
+++ src/cron/monthlyCron.js
@@ -3,17 +3,38 @@ const { arrayOfDelayedSkillsByAllUsers } = require("./arraysOfDelayedSkills");
 
 async function monthlyCron(app) {
   if (process.env.ENV === "DEV") {
-    cron.schedule("* * * * *", () => {
-      arrayOfDelayedSkillsByAllUsers(app);
-    });
+    cron.schedule(
+      "* * * * *",
+      () => {
+        arrayOfDelayedSkillsByAllUsers(app);
+      },
+      {
+        scheduled: true,
+        timezone: "Europe/Paris",
+      }
+    );
   } else if (process.env.BETA_TESTS) {
-    cron.schedule("45 16 * * *", () => {
-      arrayOfDelayedSkillsByAllUsers(app);
-    });
+    cron.schedule(
+      "45 16 * * *",
+      () => {
+        arrayOfDelayedSkillsByAllUsers(app);
+      },
+      {
+        scheduled: true,
+        timezone: "Europe/Paris",
+      }
+    );
   } else {
-    cron.schedule("0 14 1 * *", () => {
-      arrayOfDelayedSkillsByAllUsers(app);
-    });
+    cron.schedule(
+      "0 14 1 * *",
+      () => {
+        arrayOfDelayedSkillsByAllUsers(app);
+      },
+      {
+        scheduled: true,
+        timezone: "Europe/Paris",
+      }
+    );
   }
 }
 
