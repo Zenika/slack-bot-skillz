a770b2e1eb50a4c25c50513a214a565ad683ca5c
Mai-ly <mai-ly.lehoux@zenika.com>
2022-06-03 16:12:03 +0200

    :zap: new request to note skill

--- src/lib/queries/getSkillsInfosDesireAndSkillByID.js
+++ src/lib/queries/getSkillsInfosDesireAndSkillByID.js
@@ -1,15 +1,14 @@
 const GET_SKILLS_INFOS_DESIRE_AND_SKILL_BY_ID = `
 query getSkillInfosFromEmailAndID($email: String!, $id: uuid!) {
-    UserSkillDesire(where: {User: {email: {_eq: $email}}, skillId: {_eq: $id}}) {
-      Skill {
-        UserSkillDesires {
-          desireLevel
-          skillLevel
-          created_at
-        }
-      }
+  User(where: {email: {_eq: $email}}) {
+    UserSkillDesires(where: {Skill: {id: {_eq: $id}}}, limit: 1, order_by: {created_at: desc}) {
+      skillId
+      skillLevel
+      desireLevel
+      created_at
     }
-  }`;
+  }
+}`;
 
 module.exports.GET_SKILLS_INFOS_DESIRE_AND_SKILL_BY_ID =
   GET_SKILLS_INFOS_DESIRE_AND_SKILL_BY_ID;
