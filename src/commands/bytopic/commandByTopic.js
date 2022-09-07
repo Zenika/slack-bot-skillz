e3d6d09151d8f459837b3c43cf018fcd7c61911f
MailyLehoux <85115963+MailyLehoux@users.noreply.github.com>
2022-09-07 14:43:29 +0200

    :green_heart: fix ci and /by-topic command with the new datas (#90)

--- src/commands/bytopic/commandByTopic.js
+++ src/commands/bytopic/commandByTopic.js
@@ -2,6 +2,8 @@ const { responseByTopic } = require("./responseByTopic");
 const { postTwoLinesMessage } = require("../../messages/postMessages");
 const { switchCommands } = require("../../lib/utils/switchCommands");
 const { getChannelID } = require("../../lib/bolt/getSlackInformations");
+const { getAllTopics } = require("../../lib/requestsHasura/getAllTopics");
+const { getAllAgencies } = require("../../lib/requestsHasura/getAllAgencies");
 
 module.exports = {
   commandByTopic(app) {
@@ -10,13 +12,26 @@ module.exports = {
       async ({ ack, payload, context, body }) => {
         await ack();
         try {
+          const allTopicsResponse = await getAllTopics();
+          const allAgenciesResponse = await getAllAgencies();
           const responseCommand = await responseByTopic(body.text);
           const channelID = await getChannelID(body["user_id"], app, app.token);
+          let stringy = "";
+          allTopicsResponse.Topic.forEach(
+            (topic) => (stringy = stringy.concat(topic.name + ", "))
+          );
+          const allTopics = stringy.slice(0, -2);
+          stringy = "";
+          allAgenciesResponse.Agency.forEach(
+            (agency) => (stringy = stringy.concat(agency.name + ", "))
+          );
+          const allAgencies = stringy.slice(0, -2);
+
           if (responseCommand === "fail") {
             await postTwoLinesMessage(
               channelID,
               "*Command failed* :sweat:",
-              ":bulb: *Use this command like this* : /skillz-by-topic [topic] [city] \n\n :clipboard: _Topic_ available : Frontend, Backend, Agilité, Maker, Réseau, Web, Security, Microservices, Network, Ops, Devops, IA, Data, Mobile \n :globe_with_meridians: _City_ available : Paris, Nantes, Singapore, Bordeaux, Brest, Montreal, Grenoble, Lyon, Rennes, Lille",
+              `:bulb: *Use this command like this* : /skillz-by-topic [topic] [city] \n\n :clipboard: _Topic_ available : ${allTopics} \n :globe_with_meridians: _City_ available : ${allAgencies}`,
               app,
               context.botToken,
               "Response from /skillz-by-topic command"
