a145bea80a65de8eff026463bd2e152c4afa3512
MailyLehoux <85115963+MailyLehoux@users.noreply.github.com>
2022-09-21 12:08:07 +0200

    :sparkles: add new section on the app home to get the bot notifications (#93)
    * :memo: add new section in home page with a collaborator section as well

--- src/home.js
+++ src/home.js
@@ -1,42 +1,202 @@
+const { getUserEmail } = require("./lib/bolt/getSlackInformations");
+const { getBotUsers } = require("./lib/requestsHasura/getBotUsers");
 module.exports = {
   homePage(app) {
-    app.event("app_home_opened", async ({ event, client }) => {
+    app.event("app_home_opened", async ({ event, client, body }) => {
       try {
-        await client.views.publish({
-          user_id: event.user,
-          view: {
-            type: "home",
-            callback_id: "home_view",
+        const user = body["event"]["user"];
+        const collaborators = process.env.COLLABORATORS.split(";");
+        let itsACollaborator = false;
+        const botUsers = await getBotUsers();
+        const email = await getUserEmail(user, app, app.token);
 
-            /* body of the view */
-            blocks: [
-              {
-                type: "section",
-                text: {
-                  type: "mrkdwn",
-                  text: "*Welcome to your _Skillz-bot Home_* :tada:",
+        for (let i = 0; i < collaborators.length; i++) {
+          if (email == collaborators[i]) {
+            itsACollaborator = true;
+          }
+        }
+
+        if (itsACollaborator) {
+          await client.views.publish({
+            user_id: event.user,
+            view: {
+              type: "home",
+              callback_id: "home_view",
+              /* body of the view */
+              blocks: [
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*Welcome to your _Skillz-bot Home_* :tada:",
+                  },
                 },
-              },
-              {
-                type: "divider",
-              },
-              {
-                type: "section",
-                text: {
-                  type: "mrkdwn",
-                  text: "*The bot will help you to keep your Skillz profile up to date and know easily the preferred topics of the others Skillz users :technologist:*\n",
+                {
+                  type: "divider",
                 },
-              },
-              {
-                type: "section",
-                text: {
-                  type: "mrkdwn",
-                  text: "\n\n<https://skillz.zenika.com/profile|Skillz personal profile>\n:chart_with_upwards_trend: \n\n SkillZ is an application for managing the skills and competences of Zenika's different collaborators. It aims at gathering your feedbacks in order to match the different profiles with missions or trainings that correspond the best.",
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*You can see what Skillz is and the Skillz bot in the about section of this bot*\n",
+                  },
                 },
-              },
-            ],
-          },
-        });
+                {
+                  type: "image",
+                  title: {
+                    type: "plain_text",
+                    text: 'Section "About" in English version',
+                    emoji: true,
+                  },
+                  image_url:
+                    "https://media.discordapp.net/attachments/1022081920735129601/1022081958253182996/imageedit_2_2417669140.gif",
+                  alt_text: "marg",
+                },
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*Our channel :open_file_folder:*",
+                  },
+                },
+                {
+                  type: "divider",
+                },
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*#project-skillz*\nPlease post your feedbacks, questions, comments, and your issues that you encountered on the bot and on the Skillz app. And we also will post the project updates in this Slack channel.",
+                  },
+                },
+
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*Skillz App :z:*",
+                  },
+                },
+                {
+                  type: "divider",
+                },
+
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "\n\nSee your Skillz profile thanks to this link : <https://skillz.zenika.com/profile|Skillz> :chart_with_upwards_trend: \n\n ",
+                  },
+                },
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: `*Collaborator section :technologist:*\n`,
+                  },
+                },
+
+                {
+                  type: "divider",
+                },
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: `Actually, *${botUsers.length}* people have activated the bot notifications.`,
+                  },
+                },
+
+                {
+                  type: "image",
+                  title: {
+                    type: "plain_text",
+                    text: "They have this button as activated",
+                    emoji: true,
+                  },
+                  image_url:
+                    "https://media.discordapp.net/attachments/1022081920735129601/1022084020848312330/imageedit_4_9226554227.gif",
+                  alt_text: "marg",
+                },
+              ],
+            },
+          });
+        } else {
+          await client.views.publish({
+            user_id: event.user,
+            view: {
+              type: "home",
+              callback_id: "home_view",
+              /* body of the view */
+              blocks: [
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*Welcome to your _Skillz-bot Home_* :tada:",
+                  },
+                },
+                {
+                  type: "divider",
+                },
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*You can see what Skillz is and the Skillz bot in the about section of this bot*\n",
+                  },
+                },
+                {
+                  type: "image",
+                  title: {
+                    type: "plain_text",
+                    text: 'Section "About" in English version',
+                    emoji: true,
+                  },
+                  image_url:
+                    "https://media.discordapp.net/attachments/1022081920735129601/1022081958253182996/imageedit_2_2417669140.gif",
+                  alt_text: "marg",
+                },
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*Our channel :open_file_folder:*",
+                  },
+                },
+                {
+                  type: "divider",
+                },
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*#project-skillz*\nPlease post your feedbacks, questions, comments, and your issues that you encountered on the bot and on the Skillz app. And we also will post the project updates in this Slack channel.",
+                  },
+                },
+
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "*Skillz App :z:*",
+                  },
+                },
+                {
+                  type: "divider",
+                },
+
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: "\n\nSee your Skillz profile thanks to this link : <https://skillz.zenika.com/profile|Skillz> :chart_with_upwards_trend: \n\n ",
+                  },
+                },
+              ],
+            },
+          });
+        }
       } catch (error) {
         console.error("error", error);
       }
